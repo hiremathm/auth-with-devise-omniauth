188e0d64490fcc7813cb59f1423cce276abcf791
shivakumara m <shivasorab@gmail.com>
2018-07-08 18:10:55 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+                   controllers: {omniauth_callbacks: "omniauth_callbacks"}
+resources :articles
+root 'articles#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
